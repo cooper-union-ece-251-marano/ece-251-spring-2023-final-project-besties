ef66ea8fa8020e12b4b77bf7f8e33a95e112b3ce
Ridwan Hussain <114603278+Ridwan-Hussain@users.noreply.github.com>
2023-05-16 00:02:14 -0400

    Update computer.sv

--- computer.sv
+++ computer.sv
@@ -16,9 +16,9 @@
 
 `timescale 1ns/100ps
 
-`include "../Cpu/cpu.sv"
-`include "../Imem/imem.sv"
-`include "../Dmem/dmem.sv"
+`include "./Cpu/cpu.sv"
+`include "./Imem/imem.sv"
+`include "./Dmem/dmem.sv"
 
 module computer(clk, reset, writeData, dataAddr, memWrite);
 	input logic clk, reset;
